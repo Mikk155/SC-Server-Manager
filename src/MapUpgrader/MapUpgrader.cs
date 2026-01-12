d442d7cff748d3da892fdf3df1ef2c36402a8d7f
Mikk155 <martimsanchez155@gmail.com>
2026-01-12 09:50:04 -0300

    Instantiate Upgrades with Reflection

--- src/MapUpgrader/MapUpgrader.cs
+++ src/MapUpgrader/MapUpgrader.cs
@@ -1,8 +1,36 @@
 ﻿public static class Program
 {
+    private static List<IUpgrade> Upgrades = new List<IUpgrade>();
+    private static Context context = new Context();
+
     public static void Main( string[] args )
     {
-        Context context = new Context();
+        System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+        var types = assembly.GetTypes().Where( t =>
+            typeof(IUpgrade).IsAssignableFrom(t)
+            && !t.IsInterface
+            && !t.IsAbstract
+            && t.Namespace != null
+            && t.Namespace == "maps"
+        );
+
+        foreach( Type? type in types )
+        {
+            if( Activator.CreateInstance(type) is IUpgrade upgrade )
+            {
+                string[] maps = upgrade.Maps();
+
+                foreach( string map in maps )
+                {
+                    string? mapDirectory;
+
+                    if( !Program.context.AssetExists( $"maps/{map}.bsp", out mapDirectory ) || string.IsNullOrWhiteSpace( mapDirectory ) )
+                        continue;
+                }
+            }
+        }
+
         Console.WriteLine( context.SvenDirectory );
     }
 }
