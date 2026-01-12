882a90f47996b9182b6dfd63729f7e028fca7e76
Mikk155 <martimsanchez155@gmail.com>
2026-01-12 09:50:40 -0300

    Read/Write bsp with upgrades

--- src/MapUpgrader/MapUpgrader.cs
+++ src/MapUpgrader/MapUpgrader.cs
@@ -27,6 +27,48 @@
 
                     if( !Program.context.AssetExists( $"maps/{map}.bsp", out mapDirectory ) || string.IsNullOrWhiteSpace( mapDirectory ) )
                         continue;
+
+                    Sledge.Formats.Bsp.BspFile bsp = null!;
+
+                    Program.context.Map = new MapContext( map );
+
+                    using( FileStream stream = File.OpenRead( mapDirectory ))
+                    {
+                        bsp = new Sledge.Formats.Bsp.BspFile( stream );
+
+                        int i = 0;
+                        foreach( Sledge.Formats.Bsp.Objects.Entity entity in bsp.Entities )
+                        {
+                            Program.context.Map.Entities.Add( new Entity( entity.KeyValues.ToDictionary( kvp => kvp.Key, kvp => kvp.Value ), i++ ) );
+                        }
+
+                        upgrade.Upgrade( context );
+
+                        bsp.Entities.Clear();
+                    }
+
+                    using( FileStream stream = File.Create( mapDirectory ))
+                    {
+                        List<Sledge.Formats.Bsp.Objects.Entity> sledge_entities = Program.context.Map.Entities
+                            .Where( e => e.IsValid() ).Select( e =>
+                            {
+                                Sledge.Formats.Bsp.Objects.Entity sledge_entity = new Sledge.Formats.Bsp.Objects.Entity();
+
+                                foreach( KeyValuePair<string, string> kv in e.keyvalues )
+                                {
+                                    sledge_entity.KeyValues[ kv.Key ] = kv.Value;
+                                }
+
+                                return sledge_entity;
+                            } ).ToList();
+
+                        foreach( Sledge.Formats.Bsp.Objects.Entity entity in sledge_entities )
+                        {
+                            bsp.Entities.Add( entity );
+                        }
+
+                        bsp.WriteToStream( stream, bsp.Version );
+                    }
                 }
             }
         }
